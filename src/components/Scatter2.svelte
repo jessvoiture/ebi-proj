02594a7284a60d5ad924d41b2af30329cb1b5150
jessvoiture <jessicascarr989@gmail.com>
2023-05-12 07:21:10 +0100

    the struggle continues

---
+++ src/components/Scatter2.svelte
@@ -0,0 +1,81 @@
+<script>
+    import { LayerCake, ScaledSvg, Html, takeEvery } from 'layercake';
+    import { extent, bin } from 'd3-array';
+    import { scaleBand } from 'd3-scale';
+    import { format } from 'd3-format';
+
+    import Column from '../components/Column.svelte'
+    import AxisX from '../components/AxisX.html.svelte';
+    import AxisY from '../components/AxisY.html.svelte';
+
+    import calcThresholds from '../modules/calcThresholds.js';
+
+    export let data;
+
+    const f = format('.2f');
+
+    let binCount = 40;
+
+    const xKey = ['x0', 'x1'];
+    const yKey = 'length';
+
+    const domain = extent(data);
+
+    $: steps = calcThresholds(domain, binCount);
+    $: hist = bin()
+    .domain(domain)
+    .thresholds(steps);
+
+    $: slimSteps = takeEvery(steps, 7);
+</script>
+
+<style>
+/*
+  The wrapper div needs to have an explicit width and height in CSS.
+  It can also be a flexbox child or CSS grid element.
+  The point being it needs dimensions since the <LayerCake> element will
+  expand to fill it.
+*/
+    .chart-container {
+        width: 100%;
+        height: 250px;
+    }
+</style>
+
+<div class="input-container" style="position: absolute;right:10px;z-index: 9;">
+    <div class="chart-container">
+        <LayerCake
+            ssr={true}
+            percentRange={true}
+            padding={{ top: 20, right: 5, bottom: 20, left: 31 }}
+            x={xKey}
+            y={yKey}
+            xDomain={steps}
+            xScale={scaleBand().paddingInner([0])}
+            yDomain={[0, null]}
+            data={hist(data)}
+        >
+            <Html>
+                <AxisX
+                    gridlines={false}
+                    baseline={true}
+                    ticks={slimSteps}
+                    formatTick={d => +f(d)}
+                />
+                <AxisY
+                    gridlines={false}
+                    ticks={3}
+                />
+            </Html>
+
+                <ScaledSvg>
+                    <Column
+                        fill={'#fff'}
+                        stroke={'#000'}
+                        strokeWidth={1}
+                    />
+                </ScaledSvg>
+
+        </LayerCake>
+    </div>
+</div>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,13 +1,15 @@
 <script>
 	import {extent} from "d3-array";
 	import {scaleLinear, scaleOrdinal, scaleBand} from "d3-scale";
+    import * as d3 from 'd3';
+
+    import Scatter2 from "../../components/Scatter2.svelte";
     
-    export let data = {}
+    export let data
 
     // restructuring!
     let p = data.points;
     let res = p.residues;
-    let modifications = res.map(d => d.modifications)
     let pos = res.map(d => d.pos)
 
     // extract position and mod
@@ -30,10 +32,10 @@
     let unique_types = [...new Set(flattened_res.map(item => item.type))]; 
 
     // dimensions
-    let width = 1200;
-    let height = 3400;
+    let width = 1000;
+    let height = 3500;
 
-    const margin = {top: 30, left: 30, right: 30, bottom:30};
+    const margin = {top: 50, left: 150, right: 30, bottom:30};
 
     let innerWidth = width - margin.left - margin.right;
     let innerHeight = height - margin.top - margin.bottom;
@@ -54,11 +56,29 @@
 
     let legTitles = col.domain()
 
-    // make ticks
-	let xTicks = xScale.ticks(5);
-	let yTicks = yScale.ticks(unique_mods.length);
-	let xTickFormat = xScale.tickFormat(5, ".0s");
-	let yTickFormat = yScale.tickFormat(5, ".0s");
+    let xAxis;
+	let yAxis;
+	
+	$: {
+		d3.select(yAxis)
+            .call(d3.axisLeft(yScale));
+		
+		d3.select(xAxis)
+            .call(d3.axisTop(xScale))
+            .selectAll('.tick > text');
+	}
+
+    let selected_dp = undefined;
+
+    let mouse_x, mouse_y;                           
+    const setMousePosition = function(event) {       
+        mouse_x = event.clientX;
+        mouse_y = event.clientY;
+    } 
+
+    function test() {
+        console.log('hello')
+    }
 
 </script>
 
@@ -66,51 +86,79 @@
     <div class="item"><span>Name:</span> {p.name}</div>
     <div class="item"><span>Symbol:</span> {p.symbol}</div>
     <div class="item"><span>Function:</span> {p.function}</div>
-
+    <br/>
     <div class="leg">
 
         <div class=leg-title>
             {#each legTitles as lt}
-                <span style="color: {lt}"> {lt} </span><br/>
+                <div class="leg-wrapper"
+                    >
+                    <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+                    <span   class="leg-item"
+                            style="background-color:{col(lt)};"
+                            on:mouseover={test()}
+                            >
+
+                            {lt} 
+                    </span>
+                </div>
             {/each}
         </div>
 
     </div>
+
+    <!-- <Scatter2 data={data}/> -->
 </div>
 
 <svg {width} {height}>
 
-    <g class="y-axis" transform='translate(0, {margin.top})'>
-		{#each yTicks as tick}
-			<g class = "tick" transform='translate(0, {yScale(tick)})'>
-				<text x={10}>{yTickFormat(tick)}</text>
-			</g>
-		{/each}
-	</g> 
+    <g class="x-axis" transform="translate({margin.left}, {margin.top / 2})"
+		 bind:this={xAxis} />
+
+    <g class="y-axis" transform="translate({margin.left - 20}, {margin.top})"
+        bind:this={yAxis} />
 
  	<g class="dots" width={innerWidth} height={innerHeight} 
-                    transform='translate({margin.left}, {margin.top})'>
+                    transform='translate({margin.left}, {margin.top+8})'>
 		{#each flattened_res as point}
-			<circle class={point.modifications}
+			<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+			<circle class = "{point.modifications}"
+                    class:selected="{selected_dp && point.modifications == selected_dp.modifications &&
+                                      selected_dp && point.pos == selected_dp.pos  }"
                     cx={xScale(point.pos)} 
                     cy={yScale(point.modifications)} 
                     fill = {col(point.type)}
-                    r='7'/>
+                    r='7'
+                    on:mouseover={function(event) {selected_dp = point; setMousePosition(event)}} 
+                    on:mouseout={function() {selected_dp = undefined}} 
+            />
 		{/each}
 	</g>
 </svg>
 
-
-
+{#if selected_dp != undefined}            
+    <div id="tooltip" style="left: {mouse_x + 10}px; top: {mouse_y - 10}px">
+        <svg class="tooltip" width=20 height=20>
+            <g transform="translate(10,10)">
+                point={selected_dp}
+            </g>
+        </svg>Modification: {selected_dp.modifications}<br/>
+              Position: {selected_dp.pos}<br/>
+              Type: {selected_dp.type}<br/>
+              Residue: {selected_dp.residue}<br/>
+              Prob: {selected_dp.prob}
+    </div>
+{/if}
 
 <style>
     .metadata{
-        width: 20%;
+        width: 25%;
         font-family: sans-serif;
         font-size: 14pt;
         margin-left: 40px;
         margin-top: 40px;
         float: left;
+        position: fixed;
     }
 
     .item {
@@ -137,4 +185,41 @@
         box-decoration-break: clone;
         padding: 0 5px 0 5px;
     }
+
+    .leg-wrapper {
+        margin-bottom: 5px;
+    }
+
+    .x-axis{
+        position: sticky;
+    }
+
+    #tooltip {                                     
+        position: fixed;
+        background-color: white;
+        padding: 3px;
+        border: solid 1px;
+        font-family: sans-serif;
+    }
+
+    svg.tooltip {                                
+        margin: 0px;
+        padding: 0px;
+    }
+
+    svg {
+        display: block;
+        margin: auto;
+    }
+
+    circle.selected {
+        fill-opacity: 1;
+        stroke: black;
+    }
+
+    .inactive {
+        fill: lightgray;
+    }
+
+
 </style>
